3a203ca606ecedbb1daf759bf723677c0c3543ba
Da-Huang <dhuang.cn@gmail.com>
2014-08-24 18:13:46 +0800

    apache2 more standard

--- wsgi.py
+++ wsgi.py
@@ -9,11 +9,13 @@ class HelloWorld(object):
 	@cherrypy.expose
 	def index(self):
 		print 'shit'
+		'''
 		s = 1
 		for i in range(1, 100):
 			for i in range(1, 100):
 				for i in range(1, 100):
 					s *= 21
+					'''
 		return 'Hello World!'
 
 
